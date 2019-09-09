ec902cd6c8f5a4875c1aab72ee6519058d154695
Alex Lancaster <alex.lancaster@g.austincc.edu>
2019-09-09 18:58:49 -0500

    Classwork if_else.h, started switch.h

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,7 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if_else.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test get generation")
+{
+	REQUIRE(get_generation(2000) == "Centential");
+	REQUIRE(get_generation(1990) == "Millenial");
+	REQUIRE(get_generation(1970) == "Generation X");
+	REQUIRE(get_generation(1960) == "Baby Boomer");
+	REQUIRE(get_generation(1930) == "Silent Generation");
+	REQUIRE(get_generation(1776) == "Invalid");
 }
 
