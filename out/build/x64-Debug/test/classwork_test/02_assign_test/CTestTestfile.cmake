be39e289b30ac45cf1c29f5ef1f83015a975fbf3
Alex Lancaster <alex.lancaster@g.austincc.edu>
2019-09-10 14:28:36 -0500

    Started homework test - indentifiers are undefined

---
+++ out/build/x64-Debug/test/classwork_test/02_assign_test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/P2716613/source/repos/acc-cosc-1337-fall-2019-Alex-Lancaster/test/classwork_test/02_assign_test
+# Build directory: C:/Users/P2716613/source/repos/acc-cosc-1337-fall-2019-Alex-Lancaster/out/build/x64-Debug/test/classwork_test/02_assign_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(cw_02_tests "C:/Users/P2716613/source/repos/acc-cosc-1337-fall-2019-Alex-Lancaster/out/build/x64-Debug/test/classwork_test/02_assign_test/cw_02_tests.exe" "-s")
+set_tests_properties(cw_02_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/P2716613/source/repos/acc-cosc-1337-fall-2019-Alex-Lancaster/test/classwork_test/02_assign_test/CMakeLists.txt;3;add_test;C:/Users/P2716613/source/repos/acc-cosc-1337-fall-2019-Alex-Lancaster/test/classwork_test/02_assign_test/CMakeLists.txt;0;")
--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Testing get_grade_ponts entry")
+{
+	REQUIRE(get_grade_points("A") == 4);
+}
+
+TEST_CASE("Test calculate_gpa quotient")
+{
+	REQUIRE(calculate_gpa(9, 27) == 3);
+}
