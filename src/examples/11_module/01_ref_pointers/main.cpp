c47c9e469d646029a0950ff34cea2490326fbf52
Alex Lancaster <alex.lancaster@g.austincc.edu>
2019-11-18 18:17:19 -0600

    11 module ref 01

--- src/examples/11_module/01_ref_pointers/main.cpp
+++ src/examples/11_module/01_ref_pointers/main.cpp
@@ -1,8 +1,21 @@
 #include "ref_pointers.h"
+#include<iostream>
 
+using std::cout;
 
 int main() 
 {
-	
+	int num1 = 10;
+	int num2 = 20;
+
+	int& num_ref = num1;
+	int* num_ptr = &num2;
+
+	cout << "num1: \n" << num1 << "num2: \n" << num2;
+
+	ref(num_ref, num_ptr);
+
+	cout << "num1: \n" << num1 << "num2: \n" << num2;
+
 	return 0;
 }
