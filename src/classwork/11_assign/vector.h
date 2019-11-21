93f23ef3a614c8d638d9d5364bc083b0eee18c50
Alex Lancaster <alex.lancaster@g.austincc.edu>
2019-11-20 19:07:26 -0600

    Assignment 11 (classwork)

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,1 +1,18 @@
 //
+
+class Vector
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v); // copy constructor
+	Vector& operator = (const Vector& v); // copy assignment
+	Vector(Vector&& v); // Move constructor
+	Vector& operator = (Vector&& v); // Move assignment
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i) const{ return nums[i]; }
+	~Vector();
+private:
+	size_t size;
+	int* nums;
+};
