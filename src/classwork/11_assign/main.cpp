d1d1925104864fd586417823b5e4875d409a1fc3
Alex Lancaster <alex.lancaster@g.austincc.edu>
2019-11-27 18:31:38 -0600

    Fixed Classwork

--- src/classwork/11_assign/main.cpp
+++ src/classwork/11_assign/main.cpp
@@ -1,19 +1,19 @@
 #include "vector.h"
 #include<iostream>
 
-/*
-Vector get_vector();
+//Vector get_vector();
 
 int main()
 {
 	Vector<double> v(1);
 	v[0] = 2.5;
 
-	std::cout <<
+	std::cout << v[0];
 
 	return 0;
 }
 
+/*
 Vector get_vector()
 {
 	Vector v(3);
